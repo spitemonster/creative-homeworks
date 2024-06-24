5e7b9f320446e4f09f5372ff377d6e17bad977d1
Kirby Johnston <johnstk91@gmail.com>
2024-06-24 17:24:20 -0400

    updates and fixes to main navigation

--- src/js/main.js
+++ src/js/main.js
@@ -19,11 +19,20 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     menuItemsWithChildren.forEach((c) => {
-        c.addEventListener('mouseover', () => {
-            const child = c.querySelector('.wp-block-navigation-submenu')
-            const height = child.offsetHeight
+        const b = c.querySelector('button')
+        const child = c.querySelector('.wp-block-navigation-submenu')
+        const height = child.offsetHeight
 
+        c.addEventListener('mouseover', () => {
             header.style.setProperty('--submenu-height', `${height}px`)
+        })
+
+        c.addEventListener('mouseenter', () => {
+            b.setAttribute('aria-expanded', true)
+        })
+
+        c.addEventListener('mouseleave', () => {
+            b.setAttribute('aria-expanded', false)
         })
     })
 
@@ -67,4 +76,31 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         })
     }
+
+    // create responsive menu button
+
+    const headerContainer = document.querySelector(
+        '.site-header .wp-block-group:not(.is-style-breakout)'
+    )
+    const navToggle = document.createElement('button')
+    navToggle.id = 'nav-toggle'
+    navToggle.innerHTML = `
+		<span class="visually-hidden">Toggle Navigation</span>
+		<span class="open"><svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg></span>
+		<span class="closed"><svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m22 16.75c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/></svg></span>
+	`
+    const navigation = document.querySelector('.navigation-container')
+
+    navToggle.addEventListener('click', () => {
+        navigation.classList.toggle('is-menu-open')
+    })
+
+    headerContainer.append(navToggle)
+
+    // setup header hover menu display
+    // const menuItemsWithChildren = document.querySelectorAll('.wp-block-navigation-item.has-child');
+
+    // menuItemsWithChildren.forEach(i => {
+
+    // });
 })
